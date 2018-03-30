e4008166ac177c485444e960b67d38ac97f90393
weijiyang <crawling_snail@163.com>
2018-03-30 16:50:01 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # not-book
     通过vue 、 webpack 、 Node 、ES6等实现一个记事本
+## 步骤
+* npm init //初始化为一个npm项目
+* npm i webpack vue vue-loader css-loader vue-template-compiler //安装功能依赖
+* 
---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> {{text}} </div>
+</template>
+<script>
+export default {
+  data(){
+      return {
+          text : "abc"
+      }
+  }
+}
+</script>
+<style>
+
+</style>
+
+
