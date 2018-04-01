04d9cc50e23318d24b78e8300bf80d4c1976207b
weijiyang <crawling_snail@163.com>
2018-04-01 23:52:10 +0800

    extract css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +10,7 @@ const config= {
 
     entry : path.join(__dirname , "src/index.js"),
     output : {
-        filename : 'bundle.js',
+        filename : 'bundle.[hash:8].js',
         path : path.join(__dirname , "dist")
     },
     module : {
@@ -23,13 +24,6 @@ const config= {
                 loader : "babel-loader"
             },
             {
-                test : /\.css$/,
-                use : [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
                 test : /\.(gif|jpeg|png|jpg)$/,
                 use :[
                     {
@@ -39,20 +33,6 @@ const config= {
                             name :'[name]_note.[ext]'
                         }
                     }
-                ]
-            },
-            {
-                test : /\.styl$/,
-                use : [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader : 'postcss-loader',
-                        options: {
-                            sourceMap : true
-                        }
-                    },
-                    'stylus-loader'
                 ]
             }
         ]
@@ -68,6 +48,22 @@ const config= {
 }
 
 if(isDev){
+    config.module.rules.push(
+        {
+            test : /\.styl$/,
+            use : [
+                'style-loader',
+                'css-loader',
+                {
+                    loader : 'postcss-loader',
+                    options: {
+                        sourceMap : true
+                    }
+                },
+                'stylus-loader'
+            ]
+        }
+    )
     config.devtool = "#cheap-module-eval-source-map"
     config.devServer = {
         port : '8000',
@@ -81,6 +77,36 @@ if(isDev){
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{
+    config.entry = {
+        app : path.join(__dirname , 'src/index.js'),
+        vendor : ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push(
+        {
+            test : /\.styl$/,
+            use :ExtractPlugin.extract({
+                fallback : 'style-loader',
+                use:[
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options : {
+                            sourceMap : true
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            })
+        }
+    )
+    config.plugins.push(
+        new ExtractPlugin('styles.[chunkhash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            optimization:{cacheGroups:{vendor:{name:'vendor'}}}
+        })
     )
 }
 module.exports = config
